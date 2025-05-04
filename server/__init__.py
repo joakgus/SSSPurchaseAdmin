5e9547febbc78d8eac86339de0bb222c3df9761a
joakim <joakims1@yahoo.com>
2025-05-04 21:54:19 +0200

    added cors

--- server/__init__.py
+++ server/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from .items import items_bp, load_items
 from .purchases import purchases_bp
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.register_blueprint(items_bp)
     app.register_blueprint(purchases_bp)
     load_items()
